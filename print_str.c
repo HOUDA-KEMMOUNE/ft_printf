b301e432d41a251bf176bcea6331252b40df9dc9
HOUDA-KEMMOUNE <houdakemmoune2006@gmail.com>
2024-11-26 19:48:17 +0100

    update

--- print_str.c
+++ print_str.c
@@ -6,7 +6,7 @@
 /*   By: hkemmoun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 00:30:11 by hkemmoun          #+#    #+#             */
-/*   Updated: 2024/11/26 14:24:04 by hkemmoun         ###   ########.fr       */
+/*   Updated: 2024/11/26 19:47:54 by hkemmoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	print_str(char *s)
 {
 	int	count;
 
+	if (!s)
+		return (write(1, "(null)", 6));
 	count = 0;
 	while (*s)
 	{
